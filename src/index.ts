b851f4d4349c154f4cf2e71476e2656c0893085b
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2022-11-14 21:09:19 -0300

    Create models

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+    app.listen(3000, () => {
+      console.log('Server is running on http://localhost:3000');
+    });
+  })
+  .catch(() => { console.log('Error to connect with the MongoDB'); });
